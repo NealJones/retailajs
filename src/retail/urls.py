ef9482a964053ab41c18d9406eac4a366a9465b4
neal jones <alwaysworkharder@gmail.com>
2014-07-30 14:59:03 -0600

    thank you page is working, but not pretty

--- src/retail/urls.py
+++ src/retail/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
-from django import settings
+
+from django.conf import settings
 from django.conf.urls.static import static
+
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'signups.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^blog', include('blog.urls')),
+    url(r'^thankyou/$', 'signups.views.thankyou', name='thankyou'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
@@ -15,5 +19,5 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
--- src/signups/views.py
+++ src/signups/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response, RequestContext
 from .forms import SignUpForm
 
@@ -11,8 +13,17 @@ def home(request):
     if form.is_valid():
         save_it = form.save(commit=False)
         save_it.save()
+        messages.success(request, 'Thank you for joining')
+        return HttpResponseRedirect('/thankyou/')
 
     return render_to_response("signup.html",
                               locals(),
                               context_instance = RequestContext(request)
                               )
+
+
+def thankyou(request):
+    return render_to_response("thankyou.html",
+                              locals(),
+                              context_instance = RequestContext(request)
+    )

785585719786a475f42ca1ae4122227d4b6c2748
neal jones <alwaysworkharder@gmail.com>
2014-07-30 11:32:56 -0600

    break

--- src/retail/urls.py
+++ src/retail/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,3 +11,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.MEDIA_ROOT)
